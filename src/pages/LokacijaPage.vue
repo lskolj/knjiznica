c51af82694765d713f06df7893f9d5d672b4c19e
nexus301 <lovreskoljarev9@gmail.com>
2024-11-13 15:33:05 +0100

    Dodane promjene za projekt Knjižnica

--- src/pages/LokacijaPage.vue
+++ src/pages/LokacijaPage.vue
@@ -5,23 +5,23 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import * as L from 'leaflet';
-import "leaflet/dist/leaflet.css"
+import { ref, onMounted } from 'vue'
+import * as L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 
 export default {
-  setup(){
-    const initialMap = ref(null);
+  setup () {
+    const initialMap = ref(null)
 
     onMounted(() => {
-      initialMap.value = L.map('map').setView([45.3312, 14.4322], 13);
+      initialMap.value = L.map('map').setView([45.3312, 14.4322], 13)
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      }).addTo(initialMap.value);
-    });
+      }).addTo(initialMap.value)
+    })
 
-    return { initialMap };
+    return { initialMap }
   }
 }
 </script>
--- src/pages/PopisKnjigaPage.vue
+++ src/pages/PopisKnjigaPage.vue
@@ -1,11 +1,43 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <q-card>
+      <q-card-section>
+        <div class="text-h4">Popis svih knjiga</div>
+        <div class="text-subtitle2">Ovdje možete vidjeti popis svih knjiga u bazi podataka.</div>
+      </q-card-section>
+
+      <q-card-section>
+        <q-table
+        :rows="books"
+        :columns="columns"
+        row-key="id"/>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 export default {
-  // name: 'PageName',
+  data () {
+    return {
+      books: [
+        { id: 1, title: 'Knjiga 1', autor: 'Autor 1', year: 2020 },
+        { id: 2, title: 'Knjiga 2', autor: 'Autor 2', year: 2021 },
+        { id: 3, title: 'Knjiga 3', autor: 'Autor 3', year: 2022 }
+      ],
+      columns: [
+        { name: 'title', label: ' Naslov ', align: 'left', field: row => row.title },
+        { name: 'autor', label: ' Autor ', align: 'left', field: row => row.autor },
+        { name: 'year', label: ' Godina ', align: 'center', field: row => row.year }
+      ]
+    }
+  }
 }
 </script>
+
+<style scoped>
+.q-table {
+  max-height: 400px;
+  overflow: auto;
+}
+</style>
